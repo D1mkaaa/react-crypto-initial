a883cf602fc1e3df2cfec13f9d251bee6dcefa7c
Dmitriy Volkov <D_1_m_k_a-volkov@mail.ru>
2025-12-09 17:38:54 +0600

    Разработка header

--- src/context/crypto-context.jsx
+++ src/context/crypto-context.jsx
@@ -1,7 +1,6 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 import { fakeFetchCrypto, fetchAssets } from "../api";
 import { percentDifference } from "../utils";
-import { cryptoAssets } from "../data";
 
 const CryptoContext = createContext({
   assets: [],
@@ -45,3 +44,7 @@ export function CryptoContextProvider({ children }) {
 }
 
 export default CryptoContext;
+
+export function useCrypto() {
+  return useContext(CryptoContext)
+}
